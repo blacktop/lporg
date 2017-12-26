8e1b8075c18228428a3af716bb3e55b602fa70b4
blacktop <blacktop@users.noreply.github.com>
2017-12-26 11:33:23 -0700

    there might still be hope :pray:

--- launch.go
+++ launch.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
 	"os"
 	"path/filepath"
 
+	"github.com/BurntSushi/toml"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/pkg/errors"
@@ -15,11 +13,11 @@ import (
 
 // App CREATE TABLE apps (item_id INTEGER PRIMARY KEY, title VARCHAR, bundleid VARCHAR, storeid VARCHAR,category_id INTEGER, moddate REAL, bookmark BLOB)
 type App struct {
-	ItemID     int            `gorm:"column:item_id;primary_key"`
-	Title      string         `gorm:"column:title"`
-	BundleID   string         `gorm:"column:bundleid"`
-	StoreID    sql.NullString `gorm:"column:storeid"`
-	CategoryID sql.NullInt64  `gorm:"column:category_id"`
+	ItemID     int    `gorm:"column:item_id;primary_key"`
+	Title      string `gorm:"column:title"`
+	BundleID   string `gorm:"column:bundleid"`
+	StoreID    string `gorm:"column:storeid"`
+	CategoryID int    `gorm:"column:category_id"`
 	Category   Category
 	Moddate    float64 `gorm:"column:moddate"`
 	Bookmark   []byte  `gorm:"-"`
@@ -35,22 +33,26 @@ type Category struct {
 // Group CREATE TABLE groups (item_id INTEGER PRIMARY KEY, category_id INTEGER, title VARCHAR)
 type Group struct {
 	// gorm.Model
-	ID         int            `gorm:"column:item_id;primary_key"`
-	CategoryID sql.NullInt64  `gorm:"column:category_id"`
-	Title      sql.NullString `gorm:"column:title"`
+	ID         int    `gorm:"column:item_id;primary_key"`
+	CategoryID int    `gorm:"column:category_id"`
+	Title      string `gorm:"column:title"`
 }
 
 // Item CREATE TABLE items (rowid INTEGER PRIMARY KEY ASC, uuid VARCHAR, flags INTEGER, type INTEGER, parent_id INTEGER NOT NULL, ordering INTEGER)
 type Item struct {
 	RowID int `gorm:"column:rowid;primary_key"`
 	App   App
-	UUID  string        `gorm:"column:uuid"`
-	Flags sql.NullInt64 `gorm:"column:flags"`
-	Type  sql.NullInt64 `gorm:"column:type"`
+	UUID  string `gorm:"column:uuid"`
+	Flags int    `gorm:"column:flags"`
+	Type  int    `gorm:"column:type"`
 	// ParentID Group         `db:"parent_id"`
-	Group    Group         `gorm:"ForeignKey:ParentID"`
-	ParentID int           `gorm:"not null;unique;column:parent_id"`
-	Ordering sql.NullInt64 `gorm:"column:ordering"`
+	Group    Group `gorm:"ForeignKey:ParentID"`
+	ParentID int   `gorm:"not null;unique;column:parent_id"`
+	Ordering int   `gorm:"column:ordering"`
+}
+
+type AppGroups struct {
+	Groups map[string][]App
 }
 
 func checkError(err error) {
@@ -59,10 +61,25 @@ func checkError(err error) {
 	}
 }
 
+func writeTomlFile(filename string, data interface{}) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	err = toml.NewEncoder(f).Encode(data)
+
+	return err
+}
+
+func init() { log.SetLevel(log.DebugLevel) }
+
 func main() {
 
-	var item Item
+	var items []Item
 	var group Group
+	appGroups := make(map[string][]App)
 
 	// Older macOS
 	// $HOME/Library/Application\ Support/Dock/*.db
@@ -70,6 +87,7 @@ func main() {
 	// High Sierra
 	// $TMPDIR../0/com.apple.dock.launchpad/db/db
 
+	// find launchpad database
 	tmpDir := os.Getenv("TMPDIR")
 	launchDB, err := filepath.Glob(tmpDir + "../0/com.apple.dock.launchpad/db/db")
 	if err != nil {
@@ -78,6 +96,8 @@ func main() {
 	if len(launchDB) == 0 {
 		log.Fatal(errors.New("launchpad DB not found"))
 	}
+	log.Infoln("Found Launchpad DB: ", launchDB[0])
+	// open launchpad database
 	db, err := gorm.Open("sqlite3", launchDB[0])
 	if err != nil {
 		log.Fatal(err)
@@ -86,80 +106,38 @@ func main() {
 
 	// db.LogMode(true)
 
-	if err := db.First(&item, 122).Error; err != nil {
+	if err := db.Where("type = ?", "4").Find(&items).Error; err != nil {
 		log.Error(err)
 	}
 
-	db.Model(&item).Related(&item.App)
-	db.Model(&item.App).Related(&item.App.Category)
-	if err := db.First(&group, item.ParentID+1).Error; err != nil {
-		log.Error(err)
+	for _, item := range items {
+		group = Group{}
+		db.Model(&item).Related(&item.App)
+		db.Model(&item.App).Related(&item.App.Category)
+		log.Debugln("item.ParentID-1 = ", item.ParentID-1)
+		if err := db.First(&group, item.ParentID-1).Error; err != nil {
+			log.Error(err)
+		}
+		item.Group = group
+		log.Debugf("%+v\n", group)
+		if len(group.Title) > 0 {
+			appGroups[group.Title] = append(appGroups[group.Title], item.App)
+		}
 	}
+
 	// fmt.Println("--------------------------------------------------------")
-	item.Group = group
+
 	// fmt.Printf("item========================================> %+v\n", item)
-	itemJSON, _ := json.Marshal(item)
-	fmt.Println(string(itemJSON))
+	// itemJSON, _ := json.Marshal(item)
+	// fmt.Println(string(itemJSON))
 	// fmt.Printf("%+v\n", item.App)
 
-	// db, err := sqlx.Connect("sqlite3", "./launchpad.db")
-	// if err != nil {
-	// 	log.Fatal(err)
+	// write out to TOML
+	// checkError(writeTomlFile("./launchpad.toml", item))
+	// if err := db.Find(&groups).Error; err != nil {
+	// 	log.Error(err)
 	// }
-	// defer db.Close()
-
-	// checkError(db.Select(&apps, "SELECT * FROM apps ORDER BY item_id ASC"))
-	// checkError(db.Select(&categories, "SELECT * FROM categories ORDER BY rowid ASC"))
-	// checkError(db.Select(&groups, "SELECT * FROM groups ORDER BY item_id ASC"))
-	// checkError(db.Select(&items, "SELECT * FROM items ORDER BY rowid ASC"))
-
-	// for _, g := range apps {
-	// 	if g.CategoryID.Valid {
-	// 		fmt.Printf("%+v\n", g)
-	// 	} else {
-	// 		log.Error("we got some bad hombres")
-	// 	}
-	// }
-
-	// for _, g := range categories {
-	// 	fmt.Printf("%+v\n", g)
-	// }
-
-	// for _, g := range groups {
-	// 	if g.Title.Valid {
-	// 		fmt.Printf("%+v\n", g)
-	// 	} else {
-	// 		log.Error("we got some bad hombres")
-	// 	}
-	// }
-
-	// for _, g := range items {
-	// 	fmt.Printf("%+v\n", g)
-	// }
-
-	// rows, err := db.Queryx("select * from groups")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer rows.Close()
-
-	// cols, err := rows.Columns()
-	// fmt.Println(cols)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// for rows.Next() {
-	// 	app := Group{}
-	// 	err = rows.StructScan(&app)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Printf("%#v\n", app)
-	// 	// fmt.Println(itemID, title, bundleid)
-	// }
-	// err = rows.Err()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// g := make(map[string][]Group)
+	// g["Groups"] = groups
+	checkError(writeTomlFile("./groups.toml", appGroups))
 }
