874f9d92bf5629369d592649e44aa871e87973a5
blacktop <blacktop@users.noreply.github.com>
2023-09-04 10:01:49 -0600

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -58,8 +58,8 @@ func setLogLevel(verbose bool) int {
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
+		log.Error(err.Error())
 		os.Exit(1)
 	}
 }
